7125ddb7ac8802557c6014866cd4049104dcb0cc
Ellen Shea <e_shea@mit.edu>
2018-04-19 16:49:10 -0400

    created class to store image data

---
+++ ImageCoords.cpp
@@ -0,0 +1,41 @@
+#include "Arduino.h"
+#include "ImageCoords.h"
+
+ImageCoords::ImageCoords(int capacity){
+  _xCoords = new float[capacity];
+  _yCoords = new float[capacity];
+
+  _capacity = capacity;
+  _oldestIndex = 0; 
+}
+
+String ImageCoords::get1DCoords(int numMostRecent, bool isX){
+  float* arr = (isX) ? _xCoords : _yCoords;
+
+  String vals = "";
+  // i is the displacement from the NEWEST value. as i increases, our values get older
+  for(int i = 0; i < numMostRecent; i++){
+    int index = mod(_oldestIndex - 1 - i, _capacity);
+    vals += arr[index];
+    vals += ",";
+  }
+
+  return vals;
+}
+
+int ImageCoords::mod(int a, int b){
+  return (b + (a % b)) % b; //https://stackoverflow.com/questions/7594508/modulo-operator-with-negative-values
+}
+
+void ImageCoords::resetImage(){
+  _xCoords = new float[_capacity];
+  _yCoords = new float[_capacity];
+  _oldestIndex = 0;
+}
+
+void ImageCoords::addToImage(float xVal, float yVal){
+  _xCoords[_oldestIndex] = xVal;
+  _yCoords[_oldestIndex] = yVal;
+  _oldestIndex = (_oldestIndex + 1) % _capacity;
+}
+
---
+++ ImageCoords.h
@@ -0,0 +1,49 @@
+#ifndef ImageCoords_h
+#define ImageCoords_h
+#include "Arduino.h"
+
+
+class ImageCoords{
+  private:
+  float* _xCoords;
+  float* _yCoords;
+  int _capacity;
+  int _oldestIndex; // the index to next write to.
+
+  /**
+   * Takes a % b and returns the same value a % b in python would.
+   */
+  int mod(int a, int b);
+
+  public:
+
+  /**
+   * int capacity: the number of (x,y) datapoints to hold. 
+   *               ImageCoords will store the `capacity` most recent data points
+   */
+  ImageCoords(int capacity);
+
+  /**
+   * int numMostRecent: the number of most recent data points. 
+   *                    0 <= numMostRecent <= capacity
+   * bool isX .       : true to receive only X coords, false to receive only Y coords                         
+   * return           : a String of the `numMostRecent` most recent x or y coordinates
+   *                    separated by commas, with a trailing comma.
+   *                    most recent values appear first, oldest values appear last
+   */
+  String get1DCoords(int numMostRecent, bool isX);
+
+  /**
+   * Clears all current datapoints
+   */
+  void resetImage();
+
+  /**
+   * Adds the (xVal, yVal) data point to this ImageCoords
+   * float xVal : x coordinate to add
+   * float yVal : y coordinate to add
+   */
+  void addToImage(float xVal, float yVal);
+  
+};
+#endif
