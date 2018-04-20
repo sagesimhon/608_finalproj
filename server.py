7991a94c80436afb409305f195650da369c02459
Ellen Shea <e_shea@mit.edu>
2018-04-20 14:36:38 -0400

    commented servercode. Note where trailing commas are and aren't necessary

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import sqlite3
-import time
 import json
 import datetime
 
@@ -8,6 +7,11 @@ import datetime
 images_db = '__HOME__/images.db'
 
 def request_handler(request):
+    """
+    TODO pull values from request and please fill in this docstring w what request looks like
+    """
+
+    # Hard coded coordinates to draw a star
     entry_id = "star"
     x_coords = "60,20,110,10,100,60,"
     y_coords = "20,110,50,50,110,20,"
@@ -20,9 +24,10 @@ def post(entry_id, x_coords, y_coords):
     adds the x and y coords to the image specified by entry_id
 
     entry_id : string. the id of the image
-    x_coords : string in the form "x1, x2, x3, x4,...xn" NO TRAILING COMMA
+    x_coords : string in the form "x1, x2, x3, x4,...xn," MUST HAVE TRAILING COMMMA
     y_coords : ^^
     """
+
     conn = sqlite3.connect(images_db)
     c = conn.cursor()
 
@@ -39,23 +44,33 @@ def post(entry_id, x_coords, y_coords):
 
 def get_html(entry_id):
     """
-    returns html that displays the image
+    returns html that displays the image specified by `entry_id`
+    entry_id    :    a string that represents the ID of the image. 
     """
+
     conn = sqlite3.connect(images_db)
     conn.row_factory = lambda cursor, row: row[0]
     c = conn.cursor()
 
-    # [ 'x1,x2,x3,x4', 'x5,x6,x7,x8', ...] where x1 is the oldest pixel
+    # [ 'x1,x2,x3,x4,', 'x5,x6,x7,x8,', ...] where x1 is the OLDEST pixel
     all_xs = c.execute('''SELECT x_coords from image WHERE id == ? ORDER BY t ASC''', (entry_id,)).fetchall();
     all_ys = c.execute('''SELECT y_coords from image where id == ? ORDER BY t ASC''',(entry_id,)).fetchall();
 
+    # '[x1,x2,x3,x4...xn]' NOTE THAT THERE IS NO TRAILING COMMA a trailing comma will fuck up the javascript
     xCoords = ("'[" + "".join(all_xs))[:-1] + "]'"
     yCoords = ("'[" + "".join(all_ys))[:-1] + "]'"
-    # xCoords = "\'[0,1,2,3,4,100,105, 110]\'"
-    # yCoords = "\'[0,1,2,3,4,100,0, 100]\'"
     conn.close()
-    #return xCoords, yCoords
 
+    '''
+    HTML stuff:
+    <script> in head allows jquery use.
+    <canvas> is where image will be drawn
+    <span xCoords, yCoords> is where the actual coordinates will bes tored
+
+    <script> in body pulls the x and y coords from the spans. Loops through them, and connects the dots.
+
+    TODO introduce a z coordinate and instead of just connecting the lines, pick the pen up and move it appropriately
+    '''
     return '''
             <!doctype html>
                 <html lang="en">
