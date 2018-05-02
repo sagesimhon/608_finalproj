56b654a9ede1a822746d9eabfe6d1579bebf6b53
Sage Simhon <sage@RLE-EECS-MTL-DHCP-30-222.MIT.EDU>
2018-05-02 14:02:57 -0400

    added motion class

---
+++ Motion.h
@@ -0,0 +1,20 @@
+#ifndef Motion_h
+#define Motion_h
+#include "Arduino.h"
+
+class Motion
+{
+  private:
+    float canvas_x;
+    float canvas_y;
+  public:
+    float starting_pos_x;
+    float starting_pos_y;
+    Motion(float canvas_x, float canvas_y);
+    float vel_to_pos(float vel, bool isX);
+    float disp_to_pos(float disp, bool isX);
+      
+};
+
+#endif
+
