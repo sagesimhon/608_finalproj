4f7ec0d641e87ceaa83287084cda439cc50b974f
Ellen Shea <e_shea@mit.edu>
2018-05-08 18:54:36 -0400

    added deleting stuff. tested locally

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 import sqlite3
 import json
 import datetime
+import math
+import sys
 
 
 
 images_db = '__HOME__/images.db'
+# images_db = 'images.db'           # Use this path for local testing
 
 def request_handler(request):
     """
@@ -12,10 +15,13 @@ def request_handler(request):
  {'is_json': False, 'values': {'x_coords': '174.00,161.00,147.00,162.00,78.00,101.00,200.00,180.00,122.00,53.00,21.00,0.00,91.00,184.00,200.00,200.00,153.00,130.00,97.00,105.00,106.00,122.00,120.00,94.00,', 'y_coords': '38.00,55.00,48.00,62.00,37.00,0.00,0.00,4.00,137.00,183.00,186.00,5.00,0.00,0.00,0.00,0.00,96.00,117.00,84.00,58.00,48.00,38.00,40.00,65.00,', 'entry_id': 'jfusman'}, 'form': {'score': '22', 'ID': '5'}, 'args': ['x_coords', 'y_coords', 'entry_id'], 'method': 'POST'})
     
     Params that I need:
-    'x_coords' : string of 'x1,x2,x2...xk,' where x1 is the OLDEST VALUE
-    'y_coords' : ^^
-    'entry_id' : the image identifier
-    'color'    : the color the be associated with these points as a STring
+
+    'cmd'           : Optional that only occurs when requesting a delete. Value should be `DELETE`
+
+    FOR DELETING, I NEED
+    'entry_id'      :
+    'color'         : OPTIONAL
+    'num_entries'   : Number of rows to delete from db
  
     """
 
@@ -23,24 +29,40 @@ def request_handler(request):
     # post_star()
     # return get_html("star")
 
-    entry_id = "default"
 
+    values = request['form'] if 'form' in request else request['values']
+    entry_id = values['image_id']
 
-    if request['method'] == 'POST':
-        x_coords = request['form']['x_coords']
-        y_coords = request['form']['y_coords']
-        entry_id = request['form']['image_id']
-        color = request['form']['color']
-        post(entry_id, x_coords, y_coords, color);
-    else:
-    # Get requests use 'values' instead of 'form'. idk why. TODO figure this out and make everything uniform
-        assert request['method'] == 'GET' 
-        try:
-            entry_id = request['values']['image_id']
-        except:
-            pass
+    if 'cmd' in values:
+        print("removed stuff")
+        remove_coordinates(entry_id, values['num_entries'], values.get('color', 'black'))
+
+    elif request['method'] == 'POST':
+        x_coords, y_coords, color = values['x_coords'], values['y_coords'], values['color']
+        post(entry_id, x_coords, y_coords, color)
     
-    return get_html(entry_id);
+    
+    return get_html(entry_id)
+
+
+# Purpose of this function is just for testing
+# def run_insert_and_delete():
+#     entry_id, x_coords, y_coords, color = "runInsertAndDelete1", "10,20,", "10,20," , "black"
+
+
+#     # Post one row
+#     post(entry_id, x_coords, y_coords, color)
+
+#     # post another row
+#     x_coords = '30, 40,'
+#     y_coords = x_coords
+#     post(entry_id, x_coords, y_coords, color)
+
+#     # delete 1 row    
+#     remove_coordinates(entry_id, 1, color)
+#     return get_html(entry_id)
+
+
 
 def post_star():
     """
@@ -81,6 +103,32 @@ def post(entry_id, x_coords, y_coords, color):
     conn.commit();
     conn.close();
 
+def remove_coordinates(entry_id, num_entries, color=None):
+    """
+    entry_id    :   String with the value equal to the ID of the image to remove coordinates for
+    num_entries :   Integer denoting the number of full-entries to remove. The total number of coordinate removed depends on 
+                    how many were stored at every PUSH request
+    color       :   String as a valid javascript color. `remove_coordinates` will only remove the most recent `num_entries`
+                    for this color. If no color specified, remove_coordinates will remove all the `num_entries` most 
+                    recent coordinate entries
+    """
+    num_entries = int(num_entries)
+
+    conn = sqlite3.connect(images_db)
+
+    c = conn.cursor()
+
+    try:
+        c.execute('''DELETE FROM image WHERE rowid IN (SELECT rowid from image where id==? AND color==? ORDER BY t DESC LIMIT ?);''', (entry_id, color, num_entries,));
+        conn.commit()
+        conn.close()
+    except:
+        conn.close()
+        return "couldn't access image info"
+
+    return "deleted"
+
+
 
 def get_html(entry_id):
     """
@@ -97,7 +145,7 @@ def get_html(entry_id):
         rows = c.execute('''SELECT x_coords, y_coords, color from image WHERE id == ? ORDER BY t ASC''', (entry_id,)).fetchall();
         point_and_color = [list(coord) + [color] for x, y, color in rows for coord in zip(x[:-1].split(","), y[:-1].split(",")) ]
     except:
-        entry_id = 'default'
+        entry_id = '_error'
     conn.close()
 
 
@@ -150,7 +198,7 @@ def get_html(entry_id):
                         var c = document.getElementById("myCanvas");
                         var ctx = c.getContext("2d");
 
-                        if(imgID !== 'default'){
+                        if(imgID !== '_error'){
                             var pointsAndColors = ''' + str(point_and_color) + ''';
 
                             var [oldx, oldy, oldcolor] = pointsAndColors[0];
@@ -181,3 +229,32 @@ def get_html(entry_id):
                 </body>
                 </html>
             '''
+
+
+def local_test(htmlstring):
+    file = open("server.html","w")
+    file.write(htmlstring) 
+    file.close() 
+
+# Uncomment to run locally
+# if __name__ == "__main__":
+#     # To run from command line, input arguments either as:
+#     # server.py post    imgId   xVal    yVal    color
+#     # server.py get     imgID   xVal    yVal    color
+#     # server.py delete  imgId   numEntries      color
+#     # Then open the `server.html` file that should be in the same directory.
+#     _, cmd, img_id = sys.argv[:3]
+  
+#     request = {'values':{'image_id': img_id}, 'method':'GET'}
+#     r = request['values']
+
+#     if cmd == 'post':
+#         x, y, color = sys.argv[3:]
+#         r['x_coords'], r['y_coords'], r['color'] = sys.argv[3:]
+#         request['method'] = "POST"
+#     elif cmd == 'delete':
+#         r['cmd'] = 'DELETE';
+#         r['num_entries'], r['color'] = sys.argv[3:]
+    
+#     local_test(request_handler(request))
+
